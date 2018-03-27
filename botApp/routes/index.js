5c16fddeac77b6562a260ebb28b8f77b18a88a9a
Andrew Marten Medina <ammy2015@mymail.pomona.edu>
2018-03-27 11:11:28 -0700

    ok

--- botApp/routes/index.js
+++ botApp/routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+var User = models.User;
+var Task = models.Task;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
