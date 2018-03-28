ee80b65e34aecdd4d0adf56c4c4d6829ec2bd7d1
Alex <aglaze132@Gmail.com>
2018-03-28 14:12:27 -0700

    able to create google cal events with hardcode

--- botApp/routes/index.js
+++ botApp/routes/index.js
@@ -1,9 +1,32 @@
+//express server routes. we must run our app with a special command
+//since index.js and users.js are run through .bin/www
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var google = require('../google');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/setup', function(req, res) {
+  var url = google.generateAuthUrl();
+  res.redirect(url);
+})
+router.get('/google/callback', function(req, res, next) {
+  google.getToken(req.query.code)
+    .then(function (tokens) {
+      return google.createCalendarEvent(tokens, 'MY LAST TEST!! >:)', '2018-03-30')
+    })
+    .then(function() {
+      res.send('CREATED YOUR EVENT!!! :)')
+    })
+    .catch(function (err) {
+      console.log('ERROR receiving token', err);
+      res.status(500).send('Sorry we were unable to receive the Google token')
+    });
+})
+
 
 module.exports = router;
